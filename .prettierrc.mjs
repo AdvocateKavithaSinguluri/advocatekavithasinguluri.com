228e8ad23ccd6e140ff306fc6e3de492d8072c54
aditya-xq <adityavivek1998@gmail.com>
2025-09-13 00:17:10 +0530

    fix: data updates

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,18 +1,18 @@
 /** @type {import("prettier").Config} */
 export default {
-	printWidth: 120,
-	useTabs: true,
-	tabWidth: 2,
-	trailingComma: 'es5',
-	semi: false,
-	singleQuote: true,
-	plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
-	overrides: [
-		{
-			files: ['**/*.astro'],
-			options: {
-				parser: 'astro',
-			},
-		},
-	],
+    printWidth: 120,
+    useTabs: true,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    semi: false,
+    singleQuote: true,
+    plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+    overrides: [
+        {
+            files: ['**/*.astro'],
+            options: {
+                parser: 'astro',
+            },
+        },
+    ],
 }
